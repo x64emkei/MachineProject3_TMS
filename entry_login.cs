63a9942fc59b35874448f1f63b8182782c43d17d
emkei <emkei@NEOCORE-XV870D1>
2026-04-02 22:05:21 +0800

    Add project files.

---
+++ entry_login.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MachineProject3_TMS
+{
+    public partial class entry_login : Form
+    {
+        public entry_login()
+        {
+            InitializeComponent();
+        }
+    }
+}
